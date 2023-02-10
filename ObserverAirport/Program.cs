3447f30fd7db0906889d5d2b60a33300883ad74f
MICHELAZZO LEONARDO <8293569@itisrossi.edu.it>
2023-02-10 09:44:54 +0100

    Inizio observer

---
+++ ObserverAirport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverAirport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
